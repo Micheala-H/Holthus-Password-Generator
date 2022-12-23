5ba33edbfe52155c8d6904c5a1c748a24a471c64
Micheala-H <mholthus17@gmail.com>
2022-12-22 18:02:48 -0600

    added starting code and relinked files

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+ 
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
+
