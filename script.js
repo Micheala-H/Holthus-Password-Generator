5e18721c844d9ff39445392c1ee0b0682388f500
Micheala-H <mholthus17@gmail.com>
2022-12-22 22:05:29 -0600

    added variables into the generate password function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-
-
 // Assignment Code
+var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lower = "abcdefghijklmnopqrstuvwxyz"
+var numb = "0123456789"
+var special = "!@#$%^&*"
+
 var generateBtn = document.querySelector("#generate");
  
 
@@ -17,8 +20,23 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+//
+function generatePassword(){
+  var options = "";
+  var capsQ = prompt("Capital Letters? Yes or No");
+  var lowerQ = prompt("Lowercase Letters? Yes or No");
+  var numbQ = prompt("Numbers? Yes or No");
+  var specialQ = prompt("Special characters? Yes or No");
+  var passlength = prompt("How many characters?");
+  
+  //if()
+}
+
+
+
+
 for (let index = 0; index < array.length; index++) {
   const element = array[index];
   
 }
-
+return 
